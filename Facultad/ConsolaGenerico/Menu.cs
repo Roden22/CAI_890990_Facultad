45e2b6e53a709ee5177c8a56c2ecfbe4d7dc1613
Roden22 <rod.dente@gmail.com>
2021-04-24 00:01:16 -0300

    Agregado menú (genérico)

---
+++ Facultad/ConsolaGenerico/Menu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaGenerico
+{
+    public class Menu
+    {
+        // ATRIBUTOS
+        private Dictionary<string, MenuItem> _items;
+        private string _titulo = "";
+
+        // PROPIEDADES
+        public string Titulo { get => _titulo; set => _titulo = value; }
+
+        // MÉTODOS
+        public Menu(string[] claves, string[] nombres, Func<bool>[] funciones, string titulo = "")
+        {
+            if (claves == null || nombres == null || funciones == null)
+                throw new ArgumentNullException("Items de menú no pueden ser null.");
+
+            if (claves.Length != nombres.Length || claves.Length != funciones.Length)
+                throw new ArgumentException("Debe haber la misma cantidad de claves, nombres y funciones en un menú.");
+
+            _items = new Dictionary<string, MenuItem>();
+
+            for (int i = 0; i < claves.Length; i++)
+            {
+                _items.Add(claves[i], new MenuItem(nombres[i], funciones[i]));
+            }
+
+            if (titulo == "")
+            {
+                _titulo = "----------- MENU -----------";
+            }
+            else
+            {
+                _titulo = "----------- " + titulo + " -----------";
+            }
+        }
+
+        public bool Elegir()
+        {
+            string opcion;
+            MenuItem item;
+
+            opcion = Consola.LeerString("opción", false);
+
+            if (!_items.TryGetValue(opcion, out item))
+            {
+                return false;
+            }
+
+            item.Ejecutar();
+
+            return true;
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine(_titulo);
+            foreach (KeyValuePair<string, MenuItem> item in _items)
+            {
+                Console.WriteLine($"{item.Key}) {item.Value.Nombre}");
+            }
+            Console.WriteLine("----------------------------");
+        }
+
+    }
+
+    internal class MenuItem
+    {
+        // ATRIBUTOS
+        private string _nombre;
+        private Func<bool> _funcion;
+
+        // PROPIEDADES
+        public string Nombre { get => _nombre; }
+        public Func<bool> Funcion { get => _funcion; }
+
+        // MÉTODOS
+        public MenuItem(string nombre, Func<bool> funcion)
+        {
+            _nombre = nombre;
+            _funcion = funcion;
+        }
+
+        public bool Ejecutar()
+        {
+            return _funcion();
+        }
+    }
+}
--- Facultad/Facultad.Interfaz/Program.cs
+++ Facultad/Facultad.Interfaz/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsolaGenerico;
 using Facultad.Library.Entidades;
 using Facultad.Library.Excepciones;
 
@@ -10,32 +11,197 @@ namespace Facultad.Interfaz
 {
     class Program
     {
+        private static bool _salir = false;
+        private static ModeloFacultad fac;
         static void Main(string[] args)
         {
-            ModeloFacultad fac = new ModeloFacultad("FIUBA", 2);
+            
+            string[] claves = { "1", "2", "3", "4", "5", "6", "7", "8" };
+            string[] nombres = {
+                "Agregar Alumno",
+                "Agregar Empleado",
+                "EliminarAlumno",
+                "EliminarEmpleado",
+                "ModificarEmpleado",
+                "TraerAlumnos",
+                "TraerEmpleados",
+                "Salir" 
+            };
+            Func<bool>[] funciones = { 
+                AgregarAlumno,
+                AgregarEmpleado,
+                EliminarAlumno,
+                EliminarEmpleado,
+                ModificarEmpleado,
+                TraerAlumnos,
+                TraerEmpleados,
+                Salir 
+            };
 
-            fac.AgregarAlumno("Juan", "Perez", 1, new DateTime(22041991));
-            fac.AgregarAlumno("Pedro", "Gimenez", 2, new DateTime(22041991));
+            try
+            {
+                Menu menu = new Menu(claves, nombres, funciones);
+            
 
-            List<string> alumnos = fac.TraerAlumnos();
-            ImprimirLista(alumnos);
-            Console.WriteLine("");
+                fac = new ModeloFacultad("FIUBA", 2);
 
-            fac.AgregarBedel("Cacho", "Castaña", "B1", new DateTime(), 1, new DateTime());
-            fac.AgregarDirector("Carlos", "Phalen", new DateTime(), 2, new DateTime());
-            fac.AgregarDocente("Gustavo", "Schneider", new DateTime(), 3, new DateTime());
-            List<string> emps = fac.TraerEmpleados();
-            ImprimirLista(emps);
+                do
+                {
+                    menu.Imprimir();
+                    try
+                    {
+                        menu.Elegir();
+                    }
+                    catch (NotImplementedException e)
+                    {
+                        Consola.ImprimirError("Función no implementada.");
+                    }
+                    catch (Exception e)
+                    {
+                        Consola.ImprimirError(e.Message);
+                    }
 
-            Console.WriteLine("");
+                    Console.WriteLine();
+                } while (_salir == false);
 
-            fac.EliminarAlumno(1);
-            alumnos = fac.TraerAlumnos();
-            ImprimirLista(alumnos);
+            }
+            catch (Exception e)
+            {
+                Consola.ImprimirError(e.Message);
+                Console.WriteLine("\nPresionar tecla para salir...");
+                Console.ReadKey();
+            }
 
-            Console.ReadKey();
+            //fac.AgregarAlumno("Juan", "Perez", 1, new DateTime(22041991));
+            //fac.AgregarAlumno("Pedro", "Gimenez", 2, new DateTime(22041991));
+
+            //List<string> alumnos = fac.TraerAlumnos();
+            //ImprimirLista(alumnos);
+            //Console.WriteLine("");
+
+            //fac.AgregarBedel("Cacho", "Castaña", "B1", new DateTime(), 1, new DateTime());
+            //fac.AgregarDirector("Carlos", "Phalen", new DateTime(), 2, new DateTime());
+            //fac.AgregarDocente("Gustavo", "Schneider", new DateTime(), 3, new DateTime());
+            //List<string> emps = fac.TraerEmpleados();
+            //ImprimirLista(emps);
+
+            //Console.WriteLine("");
+
+            //fac.EliminarAlumno(1);
+            //alumnos = fac.TraerAlumnos();
+            //ImprimirLista(alumnos);
+
+
         }
 
+        private static bool TraerEmpleados()
+        {
+            ImprimirLista(fac.TraerEmpleados());
+            return true;
+        }
+
+        private static bool TraerAlumnos()
+        {
+            ImprimirLista(fac.TraerAlumnos());
+            return true;
+        }
+
+        private static bool ModificarEmpleado()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool EliminarEmpleado()
+        {
+            int legajo = Consola.LeerInt("legajo", false, false);
+            fac.EliminarEmpleado(legajo);
+            return true;
+        }
+
+        private static bool EliminarAlumno()
+        {
+            int codigo= Consola.LeerInt("código", false, false);
+            fac.EliminarAlumno(codigo);
+            return true;
+        }
+
+        private static bool AgregarEmpleado()
+        {
+            string[] claves = { "1", "2", "3", "4" };
+            string[] nombres = {
+                "Bedel",
+                "Docente",
+                "Directivo",
+                "Salir"
+            };
+            Func<bool>[] funciones = {
+                AgregarBedel,
+                AgregarDocente,
+                AgregarDirectivo,
+                () => false
+            };
+
+            Menu menu = new Menu(claves, nombres, funciones);
+
+            menu.Imprimir();
+            try
+            {
+                menu.Elegir();
+            }
+            catch(Exception e)
+            {
+                Consola.ImprimirError(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private static bool AgregarDirectivo()
+        {
+            string nombre = Consola.LeerString("nombre", false);
+            string apellido = Consola.LeerString("apellido", false);
+            int legajo = Consola.LeerInt("legajo", false);
+
+            fac.AgregarDirector(nombre, apellido, new DateTime(), legajo, new DateTime());
+            return true;
+        }
+
+        private static bool AgregarDocente()
+        {
+            string nombre = Consola.LeerString("nombre", false);
+            string apellido = Consola.LeerString("apellido", false);
+            int legajo = Consola.LeerInt("legajo", false);
+
+            fac.AgregarDocente(nombre, apellido, new DateTime(), legajo, new DateTime());
+            return true;
+        }
+
+        private static bool AgregarBedel()
+        {
+            string nombre = Consola.LeerString("nombre", false);
+            string apellido = Consola.LeerString("apellido", false);
+            string apodo = Consola.LeerString("apodo", false);
+            int legajo = Consola.LeerInt("legajo", false);
+
+            fac.AgregarBedel(nombre, apellido, apodo, new DateTime(), legajo, new DateTime());
+            return true;
+        }
+        private static bool AgregarAlumno()
+        {
+            string nombre = Consola.LeerString("nombre", false);
+            string apellido = Consola.LeerString("apellido", false);
+            int codigo = Consola.LeerInt("código", false);
+
+            fac.AgregarAlumno(nombre, apellido, codigo);
+
+            return true;
+        }
+
+        static bool Salir()
+        {
+            _salir = true;
+            return true;
+        }
         static void ImprimirLista(List<string> lista)
         {
             foreach(string s in lista)
